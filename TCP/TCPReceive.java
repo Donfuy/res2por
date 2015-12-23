a0ae966047fe8018f24811a127f26a50e8d6cdd5
Donfuy <donfuy@hotmail.com>
2015-12-23 18:21:50 +0000

    * Continuing.

--- TCP/TCPReceive.java
+++ TCP/TCPReceive.java
@@ -14,6 +14,7 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -38,9 +39,11 @@ public class TCPReceive implements Runnable {
         int current = 0;
         FileOutputStream fos = null;
         BufferedOutputStream bos = null;
+        ServerSocket servsock = null;
         Socket sock = null;
         try {
-            sock = new Socket(ip, port);
+            servsock = new ServerSocket(port);
+            sock = servsock.accept();
             System.out.println("Connecting...");
 
             // receive file
